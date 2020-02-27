6751a406f03b6cfc6cf5f5629e381b82c375b646
Naiden Nikolaev Petrov <58266467+naidenpetrov00@users.noreply.github.com>
2020-02-27 20:33:00 +0200

    Exercise

---
+++ Working with Abstraction - Exercise/CarsSalesman/Car.cs
@@ -0,0 +1,75 @@
+﻿namespace P02_CarsSalesman
+{
+    using System.Text;
+
+    public class Car
+    {
+        private const string offset = "  ";
+
+        private string model;
+        private Engine engine;
+        private int weight = -1;
+        private string color = "n/a";
+
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+        }
+
+        public Car(string model, Engine engine, int weight)
+            : this(model, engine)
+        {
+            this.Weight = weight;
+        }
+
+        public Car(string model, Engine engine, string color)
+            : this(model, engine)
+        {
+            this.Color = color;
+        }
+
+        public Car(string model, Engine engine, int weight, string color)
+            : this(model, engine)
+        {
+            this.Weight = weight;
+            this.Color = color;
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            private set { this.model = value; }
+        }
+
+        public Engine Engine
+        {
+            get { return this.engine; }
+            private set { this.engine = value; }
+        }
+
+        public int Weight
+        {
+            get { return this.weight; }
+            private set { this.weight = value; }
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+            private set { this.color = value; }
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("{0}:\n", this.Model);
+            sb.Append(this.Engine.ToString());
+            sb.AppendFormat("{0}Weight: {1}\n", offset, this.Weight == -1 ? "n/a" : this.Weight.ToString());
+            sb.AppendFormat("{0}Color: {1}", offset, this.Color);
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Working with Abstraction - Exercise/RawData/Car.cs
@@ -0,0 +1,62 @@
+﻿namespace P01_RawData
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Car
+    {
+        private string model;
+        private int engineSpeed;
+        private int enginePower;
+        private int cargoWeight;
+        private string cargoType;
+        private KeyValuePair<double, int>[] tires;
+        
+        public Car(string model, int engineSpeed, int enginePower, int cargoWeight, string cargoType, double tire1Pressure, int tire1Age, double tire2Pressure, int tire2Age, double tire3Pressure, int tire3age, double tire4Pressure, int tire4age)
+        {
+            this.Model = model;
+            this.EngineSpeed = engineSpeed;
+            this.EnginePower = enginePower;
+            this.CargoWeight = cargoWeight;
+            this.CargoType = cargoType;
+            this.Tires = new KeyValuePair<double, int>[] { KeyValuePair.Create(tire1Pressure, tire1Age), KeyValuePair.Create(tire2Pressure, tire2Age), KeyValuePair.Create(tire3Pressure, tire3age), KeyValuePair.Create(tire4Pressure, tire4age) };
+        }
+
+        public string Model
+        { 
+            get { return this.model; } 
+            private set { this.model = value; } 
+        }
+
+        public int EngineSpeed 
+        { 
+            get { return this.engineSpeed; }
+            private set { this.engineSpeed = value; } 
+        }
+
+        public int EnginePower 
+        { 
+            get { return this.enginePower; }
+            private set { this.enginePower = value; }
+        }
+
+        public int CargoWeight 
+        { 
+            get { return this.cargoWeight; } 
+            private set { this.cargoWeight = value; } 
+        }
+
+        public string CargoType 
+        {
+            get { return this.cargoType; } 
+            private set { this.cargoType = value; }
+        }
+
+        public KeyValuePair<double, int>[] Tires 
+        {
+            get { return this.tires; }
+            set { this.tires = value; }
+        }
+    }
+}

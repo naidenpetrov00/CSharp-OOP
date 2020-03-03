aff53ee92e4983e40680d574ec35934d21b24920
Naiden Nikolaev Petrov <58266467+naidenpetrov00@users.noreply.github.com>
2020-03-03 20:17:07 +0200

    RandomExercise

---
+++ StaticClassExercise/Mover.cs
@@ -0,0 +1,47 @@
+﻿namespace Exercise
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Mover
+    {
+        public static int[] MoveRight(int[] data)
+        {
+            var collection = new int[data.Length];
+
+            for (int i = 0; i < collection.Length; i++)
+            {
+                if (data.Length > i + 1)
+                {
+                    collection[i] = data[i + 1];
+                }
+                else
+                {
+                    collection[i] = data[i] + 1;
+                }
+            }
+
+            return collection;
+        }
+
+        public static int[] MoveLeft(int[] data)
+        {
+            var collection = new int[data.Length];
+
+            for (int i = 0; i < collection.Length; i++)
+            {
+                if (i > 0)
+                {
+                    collection[i] = data[i - 1];
+                }
+                else
+                {
+                    collection[i] = data[i] - 1;
+                }
+            }
+
+            return collection;
+        }
+    }
+}
---
+++ StaticClassExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise
+{
+    using System;
+    public class Startup
+    {
+        static void Main()
+        {
+            var arr = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine(string.Join(", ", Mover.MoveRight(arr)));
+
+            Console.WriteLine(string.Join(", ", Mover.MoveLeft(arr)));
+        }
+    }
+}

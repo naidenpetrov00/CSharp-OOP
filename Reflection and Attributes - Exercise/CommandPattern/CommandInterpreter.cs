3700a6fa780138b178dff9c588596ccbb44f7ee6
Naiden Nikolaev Petrov <58266467+naidenpetrov00@users.noreply.github.com>
2020-03-23 23:56:31 +0200

    Exercise

---
+++ Reflection and Attributes - Exercise/CommandPattern/CommandInterpreter.cs
@@ -0,0 +1,43 @@
+﻿namespace CommandPattern
+{
+    using CommandPattern.Core.Contracts;
+    using System;
+    using System.Linq;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            var result = string.Empty;
+            var argsArr = args.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            var commandName = argsArr[0].ToLower();
+            var commandArgs = argsArr.Skip(1).ToArray();
+
+            if (commandName == "Exit".ToLower())
+            {
+                var type = typeof(ExitCommand);
+                var methodToInvoke = type.GetMethod("Execute");
+
+                var classInstance = Activator.CreateInstance(type);
+
+                methodToInvoke.Invoke(classInstance, new object[] { commandArgs });
+            }
+            else if (commandName == "Hello".ToLower())
+            {
+                var type = typeof(HelloCommand);
+                var methodToInvoke = type.GetMethod("Execute");
+
+                var classInstance = Activator.CreateInstance(type);
+
+                result = methodToInvoke.Invoke(classInstance, new object[] { commandArgs }).ToString();
+            }
+            else
+            {
+                throw new ArgumentException("Unvalid command!");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Reflection and Attributes - Exercise/CommandPattern/Engine.cs
@@ -0,0 +1,36 @@
+﻿namespace CommandPattern
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using CommandPattern.Core.Contracts;
+
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                try
+                {
+                    var result = this.commandInterpreter.Read(input);
+
+                    Console.WriteLine(result);
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+        }
+    }
+}

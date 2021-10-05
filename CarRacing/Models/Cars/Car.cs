a0094ff62ea2b585c255ae0ab6b394a1f7a43e94
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2021-10-05 13:05:31 +0300

    ExamExercise

---
+++ CarRacing/Models/Cars/Car.cs
@@ -0,0 +1,117 @@
+﻿namespace CarRacing
+{
+    using CarRacing.Models.Cars.Contracts;
+    using System;
+
+    public abstract class Car : ICar
+    {
+        private const string TunedCar = "TunedCar";
+        private const string SuperCar = "SuperCar";
+
+        private string make;
+        private string model;
+        private string vin;
+        private int horsePower;
+        private double fuelAvailable;
+        private double fuelConsumptionPerRace;
+
+        public Car(string make, string model, string VIN, int horsePower, double fuelAvailable, double fuelConsuption) { }
+
+
+        public string Make
+        {
+            get { return this.make; }
+            protected set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Car make cannot be null or empty.");
+                }
+
+                this.make = value;
+            }
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            protected set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Car model cannot be null or empty.");
+                }
+
+                this.model = value;
+            }
+        }
+
+        public string VIN
+        {
+            get { return this.vin; }
+            protected set
+            {
+                if (value.Length != 17)
+                {
+                    throw new ArgumentException("Car VIN must be exactly 17 characters long.");
+                }
+
+                this.vin = value;
+            }
+        }
+
+        public int HorsePower
+        {
+            get { return this.horsePower; }
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Horse power cannot be below 0.");
+                }
+            }
+        }
+
+        public double FuelAvailable
+        {
+            get { return this.fuelAvailable; }
+            protected set
+            {
+                if (value < 0)
+                {
+                    this.fuelAvailable = 0;
+                }
+                else
+                {
+                    this.fuelAvailable = value;
+                }
+            }
+        }
+
+        public double FuelConsumptionPerRace
+        {
+            get { return this.fuelConsumptionPerRace; }
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Fuel consumption cannot be below 0.");
+                }
+
+                this.fuelConsumptionPerRace = value;
+            }
+        }
+
+        public void Drive()
+        {
+            this.FuelAvailable -= this.fuelConsumptionPerRace;
+
+            if (this.Model.GetType().Equals(TunedCar))
+            {
+                var hpAfterWear = Math.Round(this.HorsePower / 1.03);
+
+                this.HorsePower = (int)hpAfterWear;
+            }
+        }
+    }
+}
---
+++ CarRacing/Models/Maps/Map.cs
@@ -0,0 +1,30 @@
+﻿namespace CarRacing
+{
+    using CarRacing.Models.Maps.Contracts;
+    using CarRacing.Models.Racers.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class Map : IMap
+    {
+        private const string NotAvailableRacers = "Race cannot be comnpleted because both racers are not available!";
+        private string walkoverWinner = string.Format($"{0} wins the race! {1} was not available to race!");
+
+        public string StartRace(IRacer racerOne, IRacer racerTwo)
+        {
+            if (!racerOne.IsAvailable() && !racerTwo.IsAvailable())
+            {
+                return NotAvailableRacers;
+            }
+            else if (!racerOne.IsAvailable())
+            {
+               var test = walkoverWinner
+            }
+            else if (!racerTwo.IsAvailable())
+            {
+
+            }
+        }
+    }
+}

58ae909f3345f9d8286506813a3f870ee8ae28d4
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2021-10-05 22:17:40 +0300

    ExamExercise

--- Exams/C# OOP Exam - 15 August 2021/CarRacing/Models/Maps/Map.cs
+++ Exams/C# OOP Exam - 15 August 2021/CarRacing/Models/Maps/Map.cs
@@ -8,8 +8,38 @@
 
     class Map : IMap
     {
-        private const string NotAvailableRacers = "Race cannot be comnpleted because both racers are not available!";
-        private string walkoverWinner = string.Format($"{0} wins the race! {1} was not available to race!");
+        private const string NotAvailableRacers = "Race cannot be completed because both racers are not available!";
+        private const string StrictBehavior = "strict";
+        private const string AggressiveBehavior = "aggressive";
+        private const double StrictBehaviorMultiplier = 1.2;
+        private const double AggressiveBehaviorMultiplier = 1.1;
+
+        private double chanceOfWinningRacerOne;
+        private double chanceOfWinningRacerTwo;
+
+        private static void Winner(double pointsRacerOne, double pointsRacerTwo)
+        {
+
+        }
+
+        private static string WalkoverWinner(string winner, string loser)
+        {
+            return string.Format($"{0} wins the race! {1} was not available to race!", winner, loser);
+        }
+
+        private static double RacingBehaviorMultiplier(string racingBehavior)
+        {
+            if (racingBehavior == StrictBehavior)
+            {
+                return StrictBehaviorMultiplier;
+            }
+            else if (racingBehavior == AggressiveBehavior)
+            {
+                return AggressiveBehaviorMultiplier;
+            }
+
+            return 1;
+        }
 
         public string StartRace(IRacer racerOne, IRacer racerTwo)
         {
@@ -19,12 +49,19 @@
             }
             else if (!racerOne.IsAvailable())
             {
-               var test = walkoverWinner
+                return WalkoverWinner(racerTwo.Username, racerOne.Username);
             }
             else if (!racerTwo.IsAvailable())
             {
-
+                return WalkoverWinner(racerOne.Username, racerTwo.Username);
             }
+
+            chanceOfWinningRacerOne = racerOne.Car.HorsePower * racerOne.DrivingExperience * RacingBehaviorMultiplier(racerOne.RacingBehavior);
+
+            chanceOfWinningRacerTwo = racerTwo.Car.HorsePower * racerTwo.DrivingExperience * RacingBehaviorMultiplier(racerTwo.RacingBehavior);
+
+            return this.Winner
         }
+
     }
 }
